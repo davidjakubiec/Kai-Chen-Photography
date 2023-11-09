8d01b6fe61441230133c84f9ff4fc2f141ac929a
DavidJakubiec <dwjakubiec@gmail.com>
2023-11-09 14:36:14 -0800

    added captions that correspond with the current picture shown in the gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -9,9 +9,11 @@ import image4 from '../../assets/yosemite.jpg'
 
 const Gallery = () => {
     const images = [image1, image2, image3, image4];
+    const captions = ['Big Sur Bixby Bridge', 'Maui', 'San Diego Torrey Pines Overlook', 'Yosemite'];
 
     const scrollContainerRef = useRef(null);
     const [scrollPosition, setScrollPosition] = useState(0);
+    const [currImageIdx, setcurrImageIdx] = useState(0);
 
     const scrollNext = () => {
       const scrollContainer = scrollContainerRef.current;
@@ -20,7 +22,9 @@ const Gallery = () => {
   
       const newPosition = scrollPosition + containerWidth;
       if (newPosition < scrollWidth) {
+        console.log("scrolltest")
         setScrollPosition(newPosition);
+        setcurrImageIdx(currImageIdx+1);
         scrollContainer.scrollTo({ left: newPosition, behavior: 'smooth' });
       }
     };
@@ -32,9 +36,24 @@ const Gallery = () => {
       const newPosition = scrollPosition - containerWidth;
       if (newPosition >= 0) {
         setScrollPosition(newPosition);
+        setcurrImageIdx(currImageIdx-1);
         scrollContainer.scrollTo({ left: newPosition, behavior: 'smooth' });
       }
     };
+
+
+  const handleScroll = () => {
+    setScrollPosition(scrollContainerRef.current.scrollLeft);
+    setcurrImageIdx(Math.round(scrollContainerRef.current.scrollLeft/scrollContainerRef.current.clientWidth));
+  };
+
+    useEffect(() => {
+      const container = scrollContainerRef.current;
+      container.addEventListener('scroll', handleScroll);
+      return () => {
+        container.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
 
   return (
     <div>
@@ -44,7 +63,9 @@ const Gallery = () => {
         ))}
 
     </div>
+    
     <div className='button-container'>
+        <div>{captions[currImageIdx]}</div>
         <img className='gallery-arrow-buttons' src='https://www.svgrepo.com/show/486232/left-arrow-backup-2.svg' onClick={scrollPrev}></img>
         <img className='gallery-arrow-buttons' src='https://www.svgrepo.com/show/520912/right-arrow.svg' onClick={scrollNext}></img>
     </div>
--- src/pages/GalleryPage.js
+++ src/pages/GalleryPage.js
@@ -11,7 +11,6 @@ const GalleryPage = () => {
     <div >
 
         <Gallery />
-
     </div>
   )
 }
